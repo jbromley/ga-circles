9dda39dd58e0d33e8d9d0eb5480bfb0a174b7f7f
Jay Bromley <jbromley@gmail.com>
2010-11-08 01:09:44 -0800

    Corrected name of entry-point function.
    Changed the script to run GA-CIRCLES-GUI:RUN.

--- ga-circles.sh
+++ ga-circles.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sbcl --noinform --eval "(asdf:operate 'asdf:load-op :ga-circles-gui)" --eval "(ga-circles-gui:test)" --eval "(sb-ext:quit)"
+sbcl --noinform --eval "(asdf:operate 'asdf:load-op :ga-circles-gui)" --eval "(ga-circles-gui:run)" --eval "(sb-ext:quit)"
 exit 0
