4f89df86159bc4d7464fa7b87d40c8f08bfb806e
Jay Bromley <jbromley@gmail.com>
2010-11-07 23:34:18 -0800

    Made DISPLAY-WORLD accept a population.
    DISPLAY-WORLD previously accepted a list of circles. Made it accept a
    population directly to avoid having to convert a population to a list of
    circles every time I wanted to see something.

--- gui/gui.lisp
+++ gui/gui.lisp
@@ -1,11 +1,9 @@
 (in-package #:ga-circles-gui)
 
-(defun test ()
-  (let ((world (make-world)))
-    (populate-world world)
-    (display-world world)))
+(defun gui-test ()
+  (display-world (create-world)))
   
-(defun display-world (world &optional (circles '()))
+(defun display-world (world &optional (population nil))
   "Display the environment WORLD with the population in CIRCLES."
   (sdl:with-init ()
     (sdl:window (world-max-x world) (world-max-y world)
@@ -17,12 +15,14 @@
 				:color (sdl:color :r 0 :g 0 :b 192 :a 128) 
 				:alpha 255
 				:surface sdl:*default-display*))
-    (let ((color (sdl:color :r 0 :g 192 :b 0 :a 128)))
-      (dolist (c circles)
-	(sdl:draw-filled-circle-* (circle-x c) (circle-y c) (circle-radius c)
-				  :color color :alpha 255
-				  :surface sdl:*default-display*)
-	(setf color (sdl:color :r 64 :g 64 :b 64 :a 128))))
+    (let ((color (sdl:color :r 0 :g 255 :b 0 :a 255)))
+      (when (not (null population))
+	(dolist (c (reverse (mapcar #'decode-chromosome 
+				    (find-viable world population))))
+	  (sdl:draw-filled-circle-* (circle-x c) (circle-y c) (circle-radius c)
+				    :color color :alpha 255
+				    :surface sdl:*default-display*)
+	  (setf color (sdl:color :r (random 255) :g (random 255) :b (random 255) :a 32)))))
     (sdl:update-display)
     (sdl:with-events ()
       (:quit-event () t)
