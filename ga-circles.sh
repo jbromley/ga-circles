066933d7b26ebec93ac110bc1611288f77f869fc
Jay Bromley <jbromley@gmail.com>
2010-11-08 01:31:12 -0800

    Added command-line options to script launcher.
    Added command-line options to the launcher script to allow the user to specify
    the number of circles in the world and the size of the test population.

--- ga-circles.sh
+++ ga-circles.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
-sbcl --noinform --eval "(asdf:operate 'asdf:load-op :ga-circles-gui)" --eval "(ga-circles-gui:run)" --eval "(sb-ext:quit)"
+circles=50
+pop_size=50
+
+while getopts ":c:p:" opt
+do
+    case $opt in
+	c) 
+	    circles=$OPTARG
+	    ;;
+	p) 
+	    pop_size=$OPTARG
+	    ;;
+	-*)
+	    echo "Usage: $0 [-c circles] [-p population]"
+	    ;;
+    esac
+done
+
+sbcl --noinform --eval "(asdf:operate 'asdf:load-op :ga-circles-gui)" \
+    --eval "(ga-circles-gui:run :circles $circles :pop-size $pop_size)" \
+    --eval "(sb-ext:quit)"
 exit 0
