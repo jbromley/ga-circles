17cdc2205832dd2f3b51398485e1b9d7e8fa0a6e
Jay Bromley <jbromley@gmail.com>
2010-11-16 01:01:53 -0800

    Added -d parameter.
    Added the -d parameter to the launcher script to be able to choose the drawing
    mode.

--- ga-circles.sh
+++ ga-circles.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 circles=50
 pop_size=50
+draw_mode=":best"
 
-while getopts ":c:p:" opt
+while getopts ":c:p:d:" opt
 do
     case $opt in
 	c) 
@@ -11,13 +12,29 @@ do
 	p) 
 	    pop_size=$OPTARG
 	    ;;
+	d)
+	    case $OPTARG in
+		best)
+		    draw_mode=":best"
+		    ;;
+		viable)
+		    draw_mode=":draw-viable"
+		    ;;
+		all) 
+		    draw_mode=":draw-all"
+		    ;;
+		*) 
+		    echo "Usage: $0 [-c circles] [-p population] [-d best|viable|all]"
+		    ;;
+	    esac
+	    ;;
 	-*)
-	    echo "Usage: $0 [-c circles] [-p population]"
+	    echo "Usage: $0 [-c circles] [-p population] [-d best|viable|all]"
 	    ;;
     esac
 done
 
 sbcl --noinform --eval "(asdf:operate 'asdf:load-op :ga-circles-gui)" \
-    --eval "(ga-circles-gui:run :circles $circles :pop-size $pop_size)" \
+    --eval "(ga-circles-gui:run :circles $circles :pop-size $pop_size :draw-mode $draw_mode)" \
     --eval "(sb-ext:quit)"
 exit 0
